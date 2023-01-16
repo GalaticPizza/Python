0cb17b807adf82de624651700197793d67eaa18b
Nahuel <Clastik-mor13@outlook.com.ar>
2023-01-16 16:49:00 -0300

    first commit

---
+++ Curso python/Curso.py
@@ -0,0 +1,10 @@
+#OPERACIONES MATEMATICAS
+
+import math
+
+numero = int(input("ingrese un numero:"))
+
+print(f'Raiz de {numero} = {math.sqrt(numero)}')
+print(f'Seno de {numero} = {math.sin(numero)}')
+print(f'Exponencial e de {numero} = {math.exp(numero)}')
+print(f'Logartimo natural de euler {numero} = {math.log(math.exp(numero))}')
